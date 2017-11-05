22de085a0357b08f5005480ea4a81cc9cd4d3c28
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-11-05 16:36:15 -0600

    Change output extension

--- nab6-HW3.py
+++ nab6-HW3.py
@@ -4,11 +4,11 @@ import sys, time
 if __name__ == '__main__':
     start = time.clock()
     input_filepath = 'data.txt'
-    output_filepath = 'nab6-HW3.out'
+    output_filepath = 'nab6-HW3.txt'
 
     if len(sys.argv) < 3:
         print('Usage: python3.6 ./nab6-HW3.py <input_filepath> <output_filepath>')
-        print('Continuing with defaults: input = data.txt, output = nab6-HW3.out')
+        print('Continuing with defaults: input = data.txt, output = nab6-HW3.txt')
     else:
         input_filepath = sys.argv[ 1 ]
         output_filepath = sys.argv[ 2 ]
