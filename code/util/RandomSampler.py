05d4c8a1a92d7456c816cb2d036f1e8d075fad5f
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-11-12 14:36:48 -0600

    Add util class to merge, shuffle and split two datasets

---
+++ code/util/RandomSampler.py
@@ -0,0 +1,41 @@
+__author__ = 'Nestor Bermudez'
+__email__ = 'nab6@illinois.edu, nestor.bermudezs@gmail.com'
+__copyright__ = 'Copyright (C) 2017 Nestor Bermudez'
+__license__ = 'Public Domain'
+__version__ = '1.0'
+
+try:
+    from .Dataset import Dataset
+except:
+    from Dataset import Dataset
+
+import random
+
+class RandomSampler:
+    def split(train_data, test_data, valid_size = 0.1):
+        examples = train_data.examples + test_data.examples
+        classes = train_data.classes ^ test_data.classes
+        attributes = train_data.attributes ^ test_data.attributes
+        combined = Dataset.from_data(examples, classes, attributes)
+        
+        indices = list(range(0, len(examples)))
+        random.shuffle(indices)
+        
+        train = Dataset()
+        test = Dataset()
+        for i, index in enumerate(indices):
+            if i < len(examples) * valid_size:
+                test.append(examples[ index ])
+            else:
+                train.append(examples[ index ])
+        
+        return train, test
+
+if __name__ == '__main__':
+    train = Dataset.from_file('../../data/balance.scale/balance.scale.train')
+    test = Dataset.from_file('../../data/balance.scale/balance.scale.test')
+    train, test = RandomSampler.split(train, test)
+    assert type(train) == Dataset
+    assert type(test) == Dataset
+        
+        
